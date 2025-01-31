101ae19ce097e31385caf68032fc84d00c596fbb
이상원 <sangwon020214@gmail.com>
2025-01-31 22:35:32 +0900

    feat: 일지 작성한 유저와 로그인한 유저의 친구 관계 추가

--- src/dtos/mainpage.dto.js
+++ src/dtos/mainpage.dto.js
@@ -1,15 +1,16 @@
 class PostResponseDTO {
-  constructor(post) {
-      this.post_id = post.post_id; 
-      this.user_id = post.user.user_id; 
-      this.title = post.title; 
-      this.updated_at = post.updated_at; 
-      this.region = post.star.region; 
-      this.images = post.post_images.map(image => image.file_name); 
+  constructor(post, isFriend) {
+      this.post_id = post.post_id;
+      this.user_id = post.user.user_id;  //작성자 유저 아이디디
+      this.title = post.title;
+      this.updated_at = post.updated_at;
+      this.region = post.star.region;
+      this.images = post.post_images.map(image => image.file_name);
       this.user = {
-          nickname: post.user.nickname, 
-          profileImage: post.user.u_image ? post.user.u_image.file_name : null 
+          nickname: post.user.nickname,
+          profileImage: post.user.u_image ? post.user.u_image.file_name : null
       };
+      this.isFriend = isFriend;
   }
 }
 
