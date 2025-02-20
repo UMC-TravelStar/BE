8dbd83a981206ba6f20ff73c49641a9e011daf52
ri7116 <ri7116@naver.com>
2025-02-20 21:47:47 +0900

    회원정보 수정

--- src/repositories/mypage.repository.js
+++ src/repositories/mypage.repository.js
@@ -1,114 +1,118 @@
-const {PrismaClient} = require('@prisma/client');
+const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 class MyPageRepository {
-    // 유저 정보 조회
-    async findUserById(userId){
-        return await prisma.user.findFirst({
-            where: {user_id: userId},
-            select: {
-                user_id: true,
-                nickname: true,
-                password: true,
-                name: true,
-                birth: true,
-                phonenum: true,
-                email: true,
-            }
-        })
-    };
+  // 유저 정보 조회
+  async findUserById(userId) {
+    return await prisma.user.findFirst({
+      where: { user_id: userId },
+      select: {
+        user_id: true,
+        nickname: true,
+        password: true,
+        name: true,
+        birth: true,
+        phonenum: true,
+        email: true,
+      },
+    });
+  }
 
-    // 유저 정보 수정
-    async updateUserById(userId, userData){
-        return await prisma.user.update({
-            where: {user_id: userId},
-            data: userData,
-            select: {
-                user_id: true,
-                nickname: true,
-                name: true,
-                birth: true,
-                phonenum: true,
-                email: true,
-            }
-        })
-    };
-
-    // 보관 글 목록 조회
-    async findStoragedPost(userId) {
-        return await prisma.post.findMany({
-            where: { user_id: userId, storage: 2 },
-            select: {
-                post_id: true,
-                title: true,
-                created_at: true,
-                updated_at: true,
-            }
-        });
+  // 유저 정보 수정
+  async updateUserById(userId, userData) {
+    if (userData.birth && typeof userData.birth === "string") {
+      userData.birth = new Date(`${userData.birth}T00:00:00.000Z`); // 변환
     }
 
-    // 보관 글 상태 수정(보관->전체공개)
-    async updateStoragePost(postId){
-        return await prisma.post.update({
-            where: {post_id: postId},
-            data: {storage: 0},
-            select: {
-                post_id: true,
-                updated_at: true,
-            }
-        })
-    }
+    return await prisma.user.update({
+      where: { user_id: userId },
+      data: userData,
+      select: {
+        user_id: true,
+        nickname: true,
+        name: true,
+        birth: true,
+        phonenum: true,
+        email: true,
+      },
+    });
+  }
 
-    // 프로필 이미지 등록/수정 - 원래 이미지가 있는지 확인
-    async checkProfileImage(userId) {
-        return prisma.user_image.findUnique({
-            where: {
-                user_id: userId
-            }
-        })
-    };
+  // 보관 글 목록 조회
+  async findStoragedPost(userId) {
+    return await prisma.post.findMany({
+      where: { user_id: userId, storage: 2 },
+      select: {
+        post_id: true,
+        title: true,
+        created_at: true,
+        updated_at: true,
+      },
+    });
+  }
 
-    // 프로필 이미지 등록/수정 - db에 데이터 생성
-    async createImage(userId, fileUrl) {
-        return prisma.user_image.create({
-            data: {
-                user_id: userId,
-                file_name: fileUrl,
-            }
-        });
-    };
+  // 보관 글 상태 수정(보관->전체공개)
+  async updateStoragePost(postId) {
+    return await prisma.post.update({
+      where: { post_id: postId },
+      data: { storage: 0 },
+      select: {
+        post_id: true,
+        updated_at: true,
+      },
+    });
+  }
 
-    // 프로필 이미지 등록/수정 - db에 데이터 수정
-    async updateImage(userId, fileUrl) {
-        return prisma.user_image.update({
-            where: {
-                user_id: userId
-            },
-            data: {
-                file_name: fileUrl
-            }
-        });
-    };
+  // 프로필 이미지 등록/수정 - 원래 이미지가 있는지 확인
+  async checkProfileImage(userId) {
+    return prisma.user_image.findUnique({
+      where: {
+        user_id: userId,
+      },
+    });
+  }
 
-    // 프로필 이미지 삭제 - 이미지 가져오기
-    async findPostImages(userId) {
-        return prisma.user_image.findUnique({
-            where: {
-                user_id: userId
-            },
-            select: {
-                file_name: true,
-            }
-        });
-    };
+  // 프로필 이미지 등록/수정 - db에 데이터 생성
+  async createImage(userId, fileUrl) {
+    return prisma.user_image.create({
+      data: {
+        user_id: userId,
+        file_name: fileUrl,
+      },
+    });
+  }
 
-    // 프로필 이미지 삭제 - 이미지 삭제
-    async deleteImageDB(userId) {
-        return prisma.user_image.delete({
-            where: {
-                user_id: userId,
-            }
-        });
-    };
-};
+  // 프로필 이미지 등록/수정 - db에 데이터 수정
+  async updateImage(userId, fileUrl) {
+    return prisma.user_image.update({
+      where: {
+        user_id: userId,
+      },
+      data: {
+        file_name: fileUrl,
+      },
+    });
+  }
+
+  // 프로필 이미지 삭제 - 이미지 가져오기
+  async findPostImages(userId) {
+    return prisma.user_image.findUnique({
+      where: {
+        user_id: userId,
+      },
+      select: {
+        file_name: true,
+      },
+    });
+  }
+
+  // 프로필 이미지 삭제 - 이미지 삭제
+  async deleteImageDB(userId) {
+    return prisma.user_image.delete({
+      where: {
+        user_id: userId,
+      },
+    });
+  }
+}
 module.exports = new MyPageRepository();
--- src/services/mypage.service.js
+++ src/services/mypage.service.js
@@ -1,85 +1,83 @@
-const { request } = require('express');
-const MyPageRepository = require('../repositories/mypage.repository');
+const { request } = require("express");
+const MyPageRepository = require("../repositories/mypage.repository");
 const { deleteImage } = require("../middlewares/deleteImage.js");
-const { StoragePostResponseDTO } = require('../dtos/post.dto');
+const { StoragePostResponseDTO } = require("../dtos/post.dto");
 
 class MyPageService {
-    // 유저 정보 조회
-    async findUserById(userId){
-        const user = await MyPageRepository.findUserById(userId);
-        if(!user){
-            throw new Error('유저 정보가 없습니다.');
-        }
-
-        return user;
-    };
-
-    // 유저 정보 수정
-    async updateUserById(userId, userData){
-        const user = await MyPageRepository.updateUserById(userId, userData);
-        if(!user){
-            throw new Error('유저 정보 수정에 실패했습니다.');
-        }
-
-        return user;
-    };
-
-    // 보관 글 목록 조회
-    async findStoragedPost(userId) {
-        const posts = await MyPageRepository.findStoragedPost(userId);
-        if (!posts || posts.length === 0) {
-            throw new Error('보관 글 목록이 없습니다.');
-        }
-
-        return posts.map(post => new StoragePostResponseDTO(post));
+  // 유저 정보 조회
+  async findUserById(userId) {
+    const user = await MyPageRepository.findUserById(userId);
+    if (!user) {
+      throw new Error("유저 정보가 없습니다.");
     }
 
-    // 보관 글 상태 수정(보관->전체공개)
-    async updateStoragePost(postId){
-        const post = await MyPageRepository.updateStoragePost(postId);
-        if(!post){
-            throw new Error('보관 글 상태 수정에 실패했습니다.');
-        }
+    return user;
+  }
 
-        return post;
-    };
+  // 유저 정보 수정
+  async updateUserById(userId, userData) {
+    if (userData.birth && typeof userData.birth === "string") {
+      userData.birth = new Date(`${userData.birth}T00:00:00.000Z`); // ISO 8601 변환
+    }
 
-    // 프로필 이미지 등록/수정
-    async registerProfileImage(userId, fileUrl) {
-        // 1. 원래 프로필 이미지가 있는지 확인
-        const checkImage = await MyPageRepository.checkProfileImage(userId);
+    return await MyPageRepository.updateUserById(userId, userData);
+  }
 
-        // 2. 이미지가 없다면 생성 / 있다면 데이터 바꾸기
-        let image;
+  // 보관 글 목록 조회
+  async findStoragedPost(userId) {
+    const posts = await MyPageRepository.findStoragedPost(userId);
+    if (!posts || posts.length === 0) {
+      throw new Error("보관 글 목록이 없습니다.");
+    }
 
-        if (!checkImage) {
-            image = await MyPageRepository.createImage(userId, fileUrl);
-        }
-        else {
-            image = await MyPageRepository.updateImage(userId, fileUrl);
+    return posts.map((post) => new StoragePostResponseDTO(post));
+  }
 
-            await deleteImage("profile-image", [checkImage.file_name]);
-        }
+  // 보관 글 상태 수정(보관->전체공개)
+  async updateStoragePost(postId) {
+    const post = await MyPageRepository.updateStoragePost(postId);
+    if (!post) {
+      throw new Error("보관 글 상태 수정에 실패했습니다.");
+    }
 
-        return image.file_name;
-    };
+    return post;
+  }
 
-    // 프로필 이미지 삭제
-    async deleteProfileImage(userId) {
-        // 이미지 조회
-        const image = await MyPageRepository.findPostImages(userId);
+  // 프로필 이미지 등록/수정
+  async registerProfileImage(userId, fileUrl) {
+    // 1. 원래 프로필 이미지가 있는지 확인
+    const checkImage = await MyPageRepository.checkProfileImage(userId);
 
-        if (!image) {
-            return { message: "등록된 이미지가 없습니다." };
-        }
+    // 2. 이미지가 없다면 생성 / 있다면 데이터 바꾸기
+    let image;
 
-        // S3에서 이미지 삭제
-        await deleteImage("profile-image", [image.file_name]);
+    if (!checkImage) {
+      image = await MyPageRepository.createImage(userId, fileUrl);
+    } else {
+      image = await MyPageRepository.updateImage(userId, fileUrl);
 
-        // DB에서 이미지 삭제
-        await MyPageRepository.deleteImageDB(userId);
+      await deleteImage("profile-image", [checkImage.file_name]);
+    }
 
-        return { message: "이미지 삭제 완료" };
-    };
-};
+    return image.file_name;
+  }
+
+  // 프로필 이미지 삭제
+  async deleteProfileImage(userId) {
+    // 이미지 조회
+    const image = await MyPageRepository.findPostImages(userId);
+
+    if (!image) {
+      return { message: "등록된 이미지가 없습니다." };
+    }
+
+    // S3에서 이미지 삭제
+    await deleteImage("profile-image", [image.file_name]);
+
+    // DB에서 이미지 삭제
+    await MyPageRepository.deleteImageDB(userId);
+
+    return { message: "이미지 삭제 완료" };
+  }
+}
 module.exports = new MyPageService();
